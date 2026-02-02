b61b84975e4fa39ee2c3e0d69aafb2a6d5f427b8
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2026-02-02 14:37:09 -0500

    ci: Switch to vira (#15)
    From omnix

---
+++ vira.hs
@@ -0,0 +1,17 @@
+-- Pipeline configuration for Vira <https://vira.nixos.asia/>
+
+\ctx pipeline ->
+  let
+    isMain = ctx.branch == "master"
+  in
+  pipeline
+    { build.systems =
+        [ "x86_64-linux"
+        ]
+    , build.flakes =
+        [ "."
+        , "./examples/claude-sandboxed" { overrideInputs = [("landrun-nix", ".")] }
+        , "./examples/standalone" { overrideInputs = [("landrun-nix", ".")] }
+        ]
+    , signoff.enable = True
+    }
